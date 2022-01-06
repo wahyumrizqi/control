0768bfa93d3de19c4f6ced18428eac95cf41b957
wahyumrizqi <76997790+wahyumrizqi@users.noreply.github.com>
2022-01-06 14:14:09 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,23 +1,9 @@
 <!-- Statcounter code for Tsukyomi Mugen https://tsukyomimugen.blogspot.com/ on Blogger -->
-<script type="text/javascript">
+
 //<![CDATA[
 var sc_project=12698165; 
 var sc_invisible=1; 
 var sc_security="73dded06"; 
 //]]>
-</script>
-<script type="text/javascript" src="https://www.statcounter.com/counter/counter_xhtml.js" async></script>
-<noscript><div class="statcounter"><a title="Web Analytics Made Easy - Statcounter" href="https://statcounter.com/" class="statcounter"><img class="statcounter" src="https://c.statcounter.com/12698165/0/73dded06/1/" alt="Web Analytics Made Easy - Statcounter" referrerPolicy="no-referrer-when-downgrade" /></a></div></noscript>
-<!-- End of Statcounter Code -->
 
-<!-- Histats.com  START  (aync)-->
-<script type='text/javascript'>var _Hasync= _Hasync|| [];
-_Hasync.push([&#39;Histats.start&#39;, &#39;1,4514209,4,0,0,0,00010000&#39;]);
-_Hasync.push([&#39;Histats.fasi&#39;, &#39;1&#39;]);
-_Hasync.push([&#39;Histats.track_hits&#39;, &#39;&#39;]);
-(function() {
-var hs = document.createElement(&#39;script&#39;); hs.type = &#39;text/javascript&#39;; hs.async = true;
-hs.src = (&#39;//s10.histats.com/js15_as.js&#39;);
-(document.getElementsByTagName(&#39;head&#39;)[0] || document.getElementsByTagName(&#39;body&#39;)[0]).appendChild(hs);
-})();</script>
-<!-- Histats.com  END  -->
+<script type="text/javascript" src="https://www.statcounter.com/counter/counter_xhtml.js" async></script>
