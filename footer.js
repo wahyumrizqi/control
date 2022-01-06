7855ea830a8660166f7a5443a0e14eefe0104cc6
wahyumrizqi <76997790+wahyumrizqi@users.noreply.github.com>
2022-01-06 14:25:26 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,12 +1,14 @@
 <!-- Statcounter code for Tsukyomi Mugen https://tsukyomimugen.blogspot.com/ on Blogger -->
-
+<script type="text/javascript">
 //<![CDATA[
 var sc_project=12698165; 
 var sc_invisible=1; 
 var sc_security="73dded06"; 
 //]]>
-
+</script>
 <script type="text/javascript" src="https://www.statcounter.com/counter/counter_xhtml.js" async></script>
+<noscript><div class="statcounter"><a title="Web Analytics" href="https://statcounter.com/" class="statcounter"><img class="statcounter" src="https://c.statcounter.com/12698165/0/73dded06/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade" /></a></div></noscript>
+<!-- End of Statcounter Code -->
 
 
 
